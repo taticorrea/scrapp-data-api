196d9532966325d21686bba37bfeb4bff2ede961
Tatiane Corrêa <43679292+taticorrea@users.noreply.github.com>
2023-04-07 17:58:59 -0300

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,7 +1,7 @@
 import requests
 
 def delete(source:str):
-    for i in range(2854,2856):
+    for i in range(0,11095):
         url = f"http://127.0.0.1:8000/api/{source}/delete_item/{i}"
 
         payload={}
@@ -10,5 +10,7 @@ def delete(source:str):
         response = requests.request("DELETE", url, headers=headers, data=payload)
 
         print(response.text)
-source = "mercadolivre"
-delete(source)
+sources = ["mercadolivre"]
+
+for source in sources:
+    delete(source)
