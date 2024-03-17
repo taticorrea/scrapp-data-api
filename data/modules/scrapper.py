c12e237b6b37013d2e2bd40b235124a1f4be619c
Tatiane <correa.tcs@gmail.com>
2024-03-17 16:08:50 -0300

    refactor scrapper

--- data/modules/scrapper.py
+++ data/modules/scrapper.py
@@ -1,20 +1,26 @@
 from requests import get
 from pandas import DataFrame
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
 
 def getWithBs4(url: str) -> BeautifulSoup:
-    try:
-        response = get(url)
-        page_contents = response.text
-        soup = BeautifulSoup(page_contents ,'html.parser')
-        return soup
-    except:
-        pass
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    driver.implicitly_wait(3) 
+    
+    html = driver.page_source
+    
+    driver.quit()
+
+    soup = BeautifulSoup(html, "html.parser")   
+    return soup
 
 def getNextUrl(url_base, category: str, args: str, page: str) -> str:
     url = f"{url_base}{category}?{args}" + page
     return url
-
-def dataTransformation(df: DataFrame, source: str) -> DataFrame:
-    df["Fonte"] = source
-    return df
