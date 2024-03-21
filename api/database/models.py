e5485a71184f433922d2c43a3ca82853094f10f4
Tatiane <correa.tcs@gmail.com>
2024-03-21 00:32:47 -0300

    comentando outros modelos

--- api/database/models.py
+++ api/database/models.py
@@ -21,20 +21,20 @@ class Mercado(Base):
     itens = relationship('Item', back_populates='mercado')
     precos = relationship('Preco', back_populates='mercado')
 
-class Preco(Base):
-    __tablename__ = "precos"
+# class Preco(Base):
+#     __tablename__ = "precos"
     
-    id = Column(Integer, primary_key=True, index=True)
-    valor = Column(Float)
-    item_id = Column(Integer, ForeignKey('itens.id'))
-    mercado_id = Column(Integer, ForeignKey('mercados.id'))
-    item = relationship('Item', back_populates='preco')
-    mercado = relationship('Mercado', back_populates='precos')
+#     id = Column(Integer, primary_key=True, index=True)
+#     valor = Column(Float)
+#     item_id = Column(Integer, ForeignKey('itens.id'))
+#     mercado_id = Column(Integer, ForeignKey('mercados.id'))
+#     item = relationship('Item', back_populates='preco')
+#     mercado = relationship('Mercado', back_populates='precos')
 
-class Categoria(Base):
-    __tablename__ = "categorias"
+# class Categoria(Base):
+#     __tablename__ = "categorias"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    nome: str = Column(String(200))
-    descricao: str = Column(String(100))
-    itens = relationship('Item', back_populates='categoria_id')
+#     id: int = Column(Integer, primary_key=True, index=True)
+#     nome: str = Column(String(200))
+#     descricao: str = Column(String(100))
+#     itens = relationship('Item', back_populates='categoria_id')
