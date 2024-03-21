2df470eb5eea5acd5d03ae3ebc548a015309c36f
Tatiane <correa.tcs@gmail.com>
2024-03-20 22:00:45 -0300

    add crud

--- api/main.py
+++ api/main.py
@@ -1,22 +1,17 @@
 from typing import List, Optional
 from sqlalchemy.orm import Session
 from api.database.models import Item
-from api.database.database import engine, Base, get_db
+from sqlalchemy.exc import InterfaceError
 from api.database.repositories import ItemRepository
-from fastapi import FastAPI, Depends, HTTPException, status, Response
+from api.database.database import engine, Base, get_db
 from api.database.schemas import ItemRequest, ItemResponse
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/api/v2/item/", response_model=list[ItemResponse])
-def find(fonte: Optional[str] = None, id: Optional[int] = None, db: Session = Depends(get_db)):
-    itens = ItemRepository.find(db, id, fonte)                
-    return [ItemResponse.from_orm(item) for item in itens]
-
-
-@app.post("/api/v2/create-item", response_model=ItemResponse, status_code=status.HTTP_201_CREATED)
+@app.post("/api/v2/create-item", response_model=ItemResponse, status_code=status.HTTP_409_CONFLICT)
 def create(items: List[ItemRequest], db: Session = Depends(get_db)):
     db_items = []
     for item in items:
@@ -24,13 +19,30 @@ def create(items: List[ItemRequest], db: Session = Depends(get_db)):
         db_items.append(db_item)
     return Response(status_code=status.HTTP_201_CREATED)
 
+@app.get("/api/v2/item/", response_model=list[ItemResponse])
+def find(db: Session = Depends(get_db), id: int = None, fonte: str = None ):     
+    itens = ItemRepository.find(db, id, fonte)
+    return [ItemResponse.from_orm(item) for item in itens]
+
 @app.delete("/api/v2/delete-item/", status_code=status.HTTP_204_NO_CONTENT)
 def delete(id: int = None, fonte: str = None, db: Session = Depends(get_db)):
-    try:
-        ItemRepository.delete(id, fonte, db)
-        return Response(status_code=status.HTTP_200_OK)
-    except AttributeError as err:
-        print(err)
+    if id is not None and fonte is None:
+        if not ItemRepository.exists_by_id(db, id):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Item não encontrado"
+            )
+    elif fonte is not None and id is None:
+        if not ItemRepository.exists_by_font(db, id):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Fonte não encontrada"
+            )
+    else:
+        raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND, detail="Argumentos necessários"
+                    )
+    ItemRepository.delete(db, id, fonte)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 
 # @app.delete("/api/v2/delete-item/{id}", status_code=status.HTTP_204_NO_CONTENT)
