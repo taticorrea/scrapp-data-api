d302c1b1b82acbd4d50303b346b569aca420e1a8
Tatiane <correa.tcs@gmail.com>
2024-03-20 22:01:14 -0300

    ajustando funcs do repositorio

--- api/database/repositories.py
+++ api/database/repositories.py
@@ -3,37 +3,37 @@ from sqlalchemy.orm import Session
 from api.database.models import Item
 
 class ItemRepository:
+
     @staticmethod
-    def find(db: Session, id: Optional[int] = None, fonte: Optional[str] = None) -> Item:
-        if id:
+    def find(db: Session, id: int = None, fonte: str = None) -> Item:
+        if id is not None:
             itens = db.query(Item).filter(Item.id == id)
-        elif fonte:
+        elif fonte is not None:
             itens = db.query(Item).filter(Item.fonte == fonte)
         else:
             itens = db.query(Item).all()
         return itens
 
-
     @staticmethod
     def delete(db: Session, id: int = None, fonte: str = None) -> None:
-        try:
-            if id:
-                itens = db.query(Item).filter(Item.id == id)
-                itens.delete()
-                db.commit()
-            elif fonte:
-                itens = db.query(Item).filter(Item.fonte == fonte)
-                itens.delete()
-                db.commit()
-        except:
-            print("Falta parametros")
-            pass
+        if id is not None:
+            itens = db.query(Item).filter(Item.id == id)
+            itens.delete()
+            db.commit()
+        elif fonte is not None:
+            itens = db.query(Item).filter(Item.fonte == fonte)
+            itens.delete()
+            db.commit()
+        else:
+            print('fodeu')
 
     @staticmethod
-    def delete_by_id(db: Session, id: int) -> None:
-        itens = db.query(Item).filter(Item.id == id)
-        itens.delete()
-        db.commit()        
+    def exists_by_id(db: Session, id: int) -> bool:
+        return db.query(Item).filter(Item.id == id).first() is not None
+    
+    @staticmethod
+    def exists_by_font(db: Session, fonte: str) -> bool:
+        return db.query(Item).filter(Item.fonte == fonte).first() is not None     
     
     @staticmethod
     def create(db: Session, Item: Item) -> list[Item]:
@@ -49,11 +49,12 @@ class ItemRepository:
     # def find_all(db: Session) -> list[Item]:
     #     return db.query(Item).all()
     
-
-
     # @staticmethod
-    # def exists_by_id(db: Session, id: int) -> bool:
-    #     return db.query(Item).filter(Item.id == id).first() is not None
+    # def delete_by_id(db: Session, id: int) -> None:
+    #     itens = db.query(Item).filter(Item.id == id)
+    #     itens.delete()
+    #     db.commit()   
+
 
     # @staticmethod
     # def delete_all(db: Session) -> None:
